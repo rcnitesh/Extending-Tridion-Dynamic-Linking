e8f8cbe4f025b91f31a941fbc2ad08c20bd39a20
Nitesh <rc.nitesh@gmail.com>
2017-08-17 15:49:55 +0400

    Update Constant.cs

--- base/Constant.cs
+++ base/Constant.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Emaar.PierSeven.CMS.Base
+namespace Tridion.Extensions.ContentManager.Templating
 {
     public class CMSConstants
     {
